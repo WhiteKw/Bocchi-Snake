f4bd1438b0e37074efa2ec3c741d9f3d691f6966
DESKTOP-NB6G8R6\baek <kyeongwon329@gmail.com>
2023-02-13 19:08:47 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,181 @@
+let snake;
+let x, y;
+let coinX, coinY;
+let score;
+let tempDirection;
+let direction;
+let boardSize = 20;
+let isGameover;
+let game;
+let speed;
+
+// UI
+let gameoverUI = document.getElementById("gameover_ui");
+let scoreUI;
+let bestScoreUI;
+
+Init();
+
+document.onkeydown = KeyDownEventHandler;
+
+function KeyDownEventHandler(e) {
+  if (e.key === "ArrowUp" && direction !== 1) {
+    tempDirection = 0;
+  } else if (e.key === "ArrowDown" && direction !== 0) {
+    tempDirection = 1;
+  } else if (e.key === "ArrowLeft" && direction !== 3) {
+    tempDirection = 2;
+  } else if (e.key === "ArrowRight" && direction !== 2) {
+    tempDirection = 3;
+  } else if (e.key === "r" && isGameover == true) {
+    Init();
+  }
+}
+
+// 타일 생성
+function DrawBoard() {
+  let gameBoard = document.getElementById("game_board");
+  gameBoard.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
+
+  gameBoard.innerHTML = "";
+
+  for (let y = 0; y < boardSize; y++) {
+    for (let x = 0; x < boardSize; x++) {
+      let tile = document.createElement("div");
+      tile.id = `${x}-${y}`;
+      tile.className = "tile";
+      gameBoard.append(tile);
+    }
+  }
+}
+
+// 이동
+function Move() {
+  direction = tempDirection;
+
+  switch (direction) {
+    case 0: y -= 1; break;
+    case 1: y += 1; break;
+    case 2: x -= 1; break;
+    case 3: x += 1; break;
+  }
+}
+
+// 머리 추가
+function AppendHead() {
+  if (snake.length > 0) {
+    let prevHead = document.getElementById(`${snake[snake.length - 1][0]}-${snake[snake.length - 1][1]}`);
+    prevHead.style.transform = null;
+
+    if (snake.length > 1) {
+      prevHead.className = "snake";
+    }
+  }
+
+  snake.push(new Array(x, y));
+  let head =  document.getElementById(`${x}-${y}`);
+  head.className = "snake";
+  head.classList.add("head");
+
+  switch (direction) {
+    case 0: head.style.transform = "rotateZ(270deg)"; break;
+    case 1: head.style.transform = "rotateZ(90deg)"; break;
+    case 2: head.style.transform = "rotateZ(180deg)"; break;
+    case 3: head.style.transform = "rotateZ(0deg)"; break;
+  }
+}
+
+// 꼬리 제거
+function RemoveTail() {
+  let tailX = snake[0][0];
+  let tailY = snake[0][1];
+  snake.shift();
+  document.getElementById(`${tailX}-${tailY}`).className = "tile";
+  document.getElementById(`${tailX}-${tailY}`).style.transform = null;
+
+  if (snake.length > 1) {
+    let tailX = snake[0][0];
+    let tailY = snake[0][1];
+    let tail2X = snake[1][0];
+    let tail2Y = snake[1][1];
+
+    let directionX = tail2X - tailX;
+    let directionY = tail2Y - tailY;
+
+    let tail = document.getElementById(`${tailX}-${tailY}`)
+
+    if (directionX == 1) {
+      tail.style.transform = "rotateY(0deg)";
+    } else if (directionX == -1) {
+      tail.style.transform = "rotateY(180deg)";
+    } else if (directionY == 1) {
+      tail.style.transform = "rotateZ(90deg)";
+    } else if (directionY == -1) {
+      tail.style.transform = "rotateZ(-90deg)";
+    }
+
+    tail.classList.add("tail");
+  }
+}
+
+// 코인 획득 여부 체크
+function CheckEatCoin() {
+  let isEatCoin = coinX === x && coinY === y;
+
+  if (isEatCoin) {
+    score += 1;
+    SetCoin();
+  } else {
+    RemoveTail();
+  }
+}
+
+// 코인 생성
+function SetCoin() {
+  let tileArray = document.getElementsByClassName("tile");
+  let cointPostion = Math.floor(Math.random() * (tileArray.length - 0 + 1));
+  let targetId = tileArray[cointPostion].id;
+  coinX = targetId.split("-")[0] * 1;
+  coinY = targetId.split("-")[1] * 1;
+  document.getElementById(targetId).className = "coin";
+}
+
+// 충돌 체크
+function CheckCrash() {
+  let wallCrash = x + 1 > boardSize || x + 1 < 1 || y + 1 > boardSize || y + 1 < 1;
+  let bodyCrash = wallCrash ? true : document.getElementById(`${x}-${y}`).classList == "snake";
+
+  if (wallCrash || bodyCrash) {
+    clearInterval(game);
+    gameoverUI.className = "show";
+    isGameover = true;
+  }
+}
+
+function Game() {
+  Move();
+  CheckCrash();
+
+  if (!isGameover) {
+    AppendHead();
+    CheckEatCoin();
+  }
+}
+
+// 초기화
+function Init() {
+  snake = new Array();
+  score = 0;
+  x = 1;
+  y = 1;
+  tempDirection = 3;
+  direction = 3;
+  speed = 10;
+  isGameover = false;
+  gameoverUI.className = "";
+  DrawBoard();
+  AppendHead();
+  SetCoin();
+  game = setInterval(Game, 1000 / speed);
+}
+
